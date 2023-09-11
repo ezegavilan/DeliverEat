d8459c1cfb94b8b5bdbd272db70247be5fe09265
ezegavilan <ezegavilan.developer@gmail.com>
2023-09-11 16:44:29 -0300

    Payment step added. TODO: Complete form and validations

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -100,81 +100,119 @@ export function StepperWithContent() {
 
             {
                 activeStep === 0 && (
-                    <div className="mt-10 location">
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                                </svg>
-
-                            </div>
-                            <select defaultValue={"default"} id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seleccioná tu ciudad">
-                                <option value="default">Seleccioná tu ciudad</option>
-                                <option value="Córdoba">Córdoba</option>
-                                <option value="Villa María">Villa María</option>
-                                <option value="Río Cuarto">Río Cuarto</option>
-                            </select>
-                        </div>
-
-                        <div className="mt-5 columns-2">
-                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Calle" required />
-
-                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Número" required />
-                        </div>
-
-                        <div className="mt-5 columns-2">
-                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Depto" required />
-
-                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Referencia para cadete" required />
-                        </div>
-                    </div>
+                    <LocationStep />
                 )
             }
 
             {
                 activeStep === 1 && (
-                    <div className="delivery-time mt-5">
-                        <div className={`card ${(selected === 'sooner') ? 'selected' : ''}`} onClick={() => handleSelectCard('sooner')}>
-                            <span className="text-2xl"><p>¡Lo antes posible!</p></span>
-                        </div>
-
-                        <div className={`card ${(selected !== 'sooner') ? 'selected' : ''}`} onClick={() => handleSelectCard('later')}>
-                            {
-                                (selected !== 'later') && (
-                                    <span className="text-2xl"><p>Quiero elegir cuando</p></span>
-                                )
-                            }
-
-                            {
-                                (selected === 'later') && (
-                                    <div className="schedule-order">
-                                        <span className="text-2xl"><p>¿Cuando lo querés?</p></span>
-                                        <input type="datetime-local" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="¿Cuando lo querés?" required />
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </div>
+                    <DeliveryTimeStep selected={selected} handleSelectCard={handleSelectCard} />
                 )
             }
 
             {
                 activeStep === 2 && (
-                    <div className="mt-10 location">
-                        <p>STEP 0</p>
-                    </div>
+                    <PayMethodStep />
                 )
             }
 
             <div className="mt-16 flex justify-between">
                 <Button onClick={handlePrev} disabled={isFirstStep}>
-                    Prev
+                    Anterior
                 </Button>
                 <Button onClick={handleNext} disabled={isLastStep}>
-                    Next
+                    Siguiente
                 </Button>
             </div>
         </div>
     );
+}
+
+const LocationStep = () => {
+    return (
+        <div className="mt-10 location">
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                    </svg>
+
+                </div>
+                <select defaultValue={"default"} id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seleccioná tu ciudad">
+                    <option value="default">Seleccioná tu ciudad</option>
+                    <option value="Córdoba">Córdoba</option>
+                    <option value="Villa María">Villa María</option>
+                    <option value="Río Cuarto">Río Cuarto</option>
+                </select>
+            </div>
+
+            <div className="mt-5 columns-2">
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Calle" required />
+
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Número" required />
+            </div>
+
+            <div className="mt-5 columns-2">
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Depto" required />
+
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Referencia para cadete" required />
+            </div>
+        </div>
+    )
+}
+
+const DeliveryTimeStep = ({ selected, handleSelectCard }: any) => {
+    return (
+        <div className="delivery-time mt-5">
+            <div className={`card ${(selected === 'sooner') ? 'selected' : ''}`} onClick={() => handleSelectCard('sooner')}>
+                <span className="text-2xl"><p>¡Lo antes posible!</p></span>
+            </div>
+
+            <div className={`card ${(selected !== 'sooner') ? 'selected' : ''}`} onClick={() => handleSelectCard('later')}>
+                {
+                    (selected !== 'later') && (
+                        <span className="text-2xl"><p>Quiero elegir cuando</p></span>
+                    )
+                }
+
+                {
+                    (selected === 'later') && (
+                        <div className="schedule-order">
+                            <span className="text-2xl"><p>¿Cuando lo querés?</p></span>
+                            <input type="datetime-local" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="¿Cuando lo querés?" required />
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+const PayMethodStep = () => {
+    return (
+        <div className="payment-container mt-10">
+            <div className="payment mx-5">
+                <div className="payment-method">
+                    <span>
+                        <p>¿Cómo vas a pagar?</p>
+                    </span>
+                </div>
+            </div>
+
+            <div className="payment-amount">
+                <span>
+                    <p>Total</p>
+                </span>
+
+                <div className="separator mt-2"></div>
+
+                <div className="amount-info">
+                    <span>
+                        <p>$ 6.000</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
 }
